b49eebc21fe083806006ad4d844bccaeecc2aa2d
Kith7 <grzegorz.jaszczak@gmail.com>
2022-12-05 19:31:37 +0100

    add category and categories

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./categories.styles.scss";
+import Categories from "./components/categories/categories.component";
+
+const App = () => {
+  return <Categories />;
+};
+
+export default App;
