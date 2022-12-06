83beba13da77018a1111a7ff8fef526ea5f1b579
Kith7 <grzegorz.jaszczak@gmail.com>
2022-12-06 22:53:53 +0100

    add router and home route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./categories.styles.scss";
-import Categories from "./components/categories/categories.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Categories />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
