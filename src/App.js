e2b45e67336ed4832522db561e1aa33d5902d0ee
Kith7 <grzegorz.jaszczak@gmail.com>
2022-12-08 12:36:00 +0100

    add google auth and save user data in firebase db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./categories.styles.scss";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import { SignIn } from "./routes/sing-in/sign-in.component";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Home />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
   );
