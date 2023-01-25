9adec8cc7a422849e52dbf0db730b5494a162370
Kith7 <grzegorz.jaszczak@gmail.com>
2023-01-25 13:08:38 +0100

    add credit card style,  finish spinner

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import Button from "../button/button.component";
+
 export const PaymentFormContainer = styled.div`
   height: 300px;
   display: flex;
@@ -8,7 +10,11 @@ export const PaymentFormContainer = styled.div`
   justify-content: center;
 `;
 
-export const FormContainer = styled.div`
-  height: 300px;
-  min-width: 360px;
+export const FormContainer = styled.form`
+  min-width: 500px;
+`;
+
+export const PaymentButton = styled(Button)`
+  margin-left: auto;
+  margin-top: 30px;
 `;
